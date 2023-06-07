71ba1ad8a9f5e5e299a9de9daadd60397cd101ac
Yousef <yusufkhattab3@gmail.com>
2023-06-07 04:00:14 +0300

    semi-final

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')+1):
-    print(f"{chr(letter)}", end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')+1):
-    if chr(letter) not in ('q', 'e'):
-        print(f"{chr(letter)}", end="")
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print("{}".format(chr(i)), end="")
