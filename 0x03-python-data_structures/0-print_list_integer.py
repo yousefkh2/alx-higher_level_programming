aef32711e6eda0556a6933fc3031679e237481fa
Yousef <yusufkhattab3@gmail.com>
2023-06-13 00:04:47 +0300

    last

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """print integer in list
-    
+
     Args:
         my_list: the list to print from
-    
+
     Returns:
         Null
     """
